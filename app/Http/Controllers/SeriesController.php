835b508791a17e8fd7da5a1b87a018140b9d8e67
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-03-23 14:41:08 -0300

    :up:

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,33 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
+use Cassandra\Timestamp;
+use http\Exception\RuntimeException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class SeriesController extends Controller
 {
-    public function index(): View
+    public function index(Serie $series): View
     {
-        $series = DB::select('SELECT nome FROM series');
-
-
-        return view('series.index')->with('series', $series);
+        return view('series.index')->with('series', $series::all());
     }
 
     public function create(): View
     {
-        return \view('series.create');
+        return view('series.create');
     }
 
     public function store(Request $request)
     {
         $nomeSeries = $request->input('nome');
-
-        if (DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSeries])) {
-            return "Ok";
-        } else {
-            return "Deu ERRO";
-        }
+        $serie = new Serie();
+        $serie->nome = $nomeSeries;
+        $serie->save();
+        return redirect('/series');
     }
 }
