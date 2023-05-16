d957dbcf346f019aa8c390e33c8dfb052ef0dd63
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-05-15 22:54:44 -0300

    :up:

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Serie extends Model
 {
     use HasFactory;
 
     protected $fillable = ['nome'];
+    protected $with = ['seasons'];
+
+    public function seasons(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Season::class, 'series_id');
+    }
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered', function (Builder $queryBuilder) {
+            $queryBuilder->orderBy('nome');
+        });
+    }
 }
